365d2b76753c8ef9736ffeb1d4def7d7ca395ca3
Brian Norman <brian.norman@jetbrains.com>
2024-07-10 14:15:46 -0500

    Drop language version 1.4 and 1.5: JPS test updates

--- jps/jps-plugin/jps-tests/test/org/jetbrains/kotlin/jps/build/KotlinJpsBuildTest.kt
+++ jps/jps-plugin/jps-tests/test/org/jetbrains/kotlin/jps/build/KotlinJpsBuildTest.kt
@@ -283,7 +283,7 @@ open class KotlinJpsBuildTest : KotlinJpsBuildTestBase() {
         assertEquals(1, myProject.modules.size)
         val module = myProject.modules.first()
         val args = module.kotlinCompilerArguments
-        args.apiVersion = "1.4"
+        args.apiVersion = "1.6"
         myProject.kotlinCommonCompilerArguments = args
 
         buildAllModules().assertSuccessful()
